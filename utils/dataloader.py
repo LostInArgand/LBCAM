d90711f49066d5c011cb427c36c315d2da048c94
Praditha <alwispraditha20@gmail.com>
2024-03-16 15:18:03 +0530

    Added a Jupyter Notebook to test wavelet functions. Added a main Notebook to implement the LBCAM algorithm. Did a bug fix in dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -10,7 +10,8 @@ class dtLoader:
                  STRIDE = 1,
                  WINDOW_LENGTH = 8,
                  FRONT_MARGIN = 2,
-                 REAR_MARGIN = 2):
+                 REAR_MARGIN = 2,
+                 SENSORS=[]):
 
         """
         Splits the signals into windows
@@ -21,6 +22,7 @@ class dtLoader:
             WINDOW_LENGTH   : length of the required window in seconds
             FRONT_MARGIN    : Required offset before a fetal kick (in seconds)
             REAR_MARGIN     : Required offset after a fetal kick (in seconds)
+            SENSORS         : List of the required sensor channels
         """
 
         self.SOURCE = SOURCE
@@ -29,6 +31,7 @@ class dtLoader:
         self.WINDOW_LENGTH = WINDOW_LENGTH
         self.FRONT_MARGIN = FRONT_MARGIN
         self.REAR_MARGIN = REAR_MARGIN
+        self.SENSORS = SENSORS
         self.window_list = []
         self.kick_count_list = []
 
@@ -50,6 +53,7 @@ class dtLoader:
         for i in range(10 * self.SAMPLE_FREQ, n - 10 * self.SAMPLE_FREQ):
             if df["state"][i] == 0:
                 count += 1
+                cum_sum[i] = cum_sum[i - 1]
             elif df["state"][i] >= 3:
                 if count >= 8:
                     cum_sum[i] = cum_sum[i - 1] + 1
@@ -93,12 +97,14 @@ class dtLoader:
                 # kick in rear margin and also in middle => increase 1
                 case_inc_kick += 1
 
-            self.window_list.append(df.iloc[i:i + self.WINDOW_LENGTH * self.SAMPLE_FREQ, :].copy())
+            raw_window = df.iloc[i:i + self.WINDOW_LENGTH * self.SAMPLE_FREQ, :].copy()
+            n_window = self.remove_DC(raw_window)
+            self.window_list.append(n_window)
             self.kick_count_list.append(cum_sum[i + self.WINDOW_LENGTH * self.SAMPLE_FREQ] - cum_sum[i + self.FRONT_MARGIN * self.SAMPLE_FREQ] + case_inc_kick - case_dec_kick)
 
             i += self.STRIDE * self.SAMPLE_FREQ
 
-        return cum_sum[n-1]
+        return cum_sum[n - 10 * self.SAMPLE_FREQ - 1]
 
     def split_windows(self):
 
@@ -106,9 +112,7 @@ class dtLoader:
         Read the recodings and split those into windows
         Args:
         Returns:
-            self.window_list            : Set of generated windows
-            self.kick_count_list        : Number of kicks in each window
-            counts                      : Dictionary containing number of windows with the given number of kicks
+            counts                      : Dictionary which gives the number of windows with the given number of kicks
         """
         dir_list = os.listdir(self.SOURCE)
         print('-' * 80)
@@ -141,62 +145,53 @@ class dtLoader:
         print('-' * 80)
         print(f'Total number of kicks : {kick_count}')
 
-        assert(len(self.window_list) == len(self.kick_count_list))
-        return self.window_list, self.kick_count_list, counts
 
-def createClasses(window_list, kick_count_list, sensors, diff):
-    """
-    Removes the DC component and outputs the data as a tuple and a dictionary
-    Args:
-        window_list             : list of windowed data
-        kick_count_list         : list of kick count data
-        sensors                 : list of sensors
-        diff                    : (No. of no kick windows) - (No. of kick windows)
-    Returns:
-        data                    : output data as a tuple (window, ground truth)
-        classes                 : output data as a dictionary where key is the                            ground truth and sensor readings are values
-    """
-    new_window_list = []
-    for i in range(len(kick_count_list)):
-        temp = np.array(window_list[i][sensors], dtype=np.float32)
+        assert(len(self.window_list) == len(self.kick_count_list))
+        return counts
+
+    def remove_DC(self, u_window):
+        """
+        Removes the DC component
+        Args:
+            u_window                : data window
+        Returns:
+            temp.T                  : recordings without the DC components -> shape(sensors, amplitudes)
+        """
+        temp = u_window[self.SENSORS]
         mean = np.mean(temp, axis=0)
         temp -= np.mean(temp, axis=0)
-        new_window_list.append(temp.T)
-    new_window_list = np.array(new_window_list, dtype=np.float32)
+        return temp.T
 
-    path = "drive/MyDrive/Smart Monitoring for Biomedical Applications/Datasets/Time_Domain_Processed/Randomly_Dropped_Windows/"
-    os.mkdir(path)
-    w_num = 0
-    classes = defaultdict(list)
-    for i in range(len(kick_count_list)):
-        if kick_count_list[i] == 0:
-            if random.random() > 0.06 and diff > 0:
-                np.save(path + 'window' + str(w_num) + '.npy', new_window_list[i])
-                w_num += 1
-                diff -= 1
-                continue
+    def drop_zero_kicks(self, diff, path):
+        """
+        Balances the dataset by dropping few zero kick windows randomly
+        Args:
+            path                    : File path to save dropped zero kick windows
+            diff                    : (No. of no kick windows) - (No. of kick windows)
+        Returns:
+            new_window_list         : numpy array with modified windows
+            new_kicks_list          : numpy array with ground truth
+        """
+
+        os.mkdir(path)
+        new_window_list = []
+        new_kicks_list = []
+        w_num = 0
+        for i in range(len(self.kick_count_list)):
+            if self.kick_count_list[i] == 0:
+                if random.random() > 0.06 and diff > 0:
+                    np.save(path + 'window' + str(w_num) + '.npy', self.window_list[i])
+                    w_num += 1
+                    diff -= 1
+                else:
+                    new_window_list.append(self.window_list[i])
+                    new_kicks_list.append(0)
+            elif self.kick_count_list[i] > 2:
+                new_window_list.append(self.window_list[i])
+                new_kicks_list.append(3)
             else:
-                classes[0].append(new_window_list[i])
-        elif kick_count_list[i] == 1:
-            classes[1].append(new_window_list[i])
-        elif kick_count_list[i] == 2:
-            classes[2].append(new_window_list[i])
-        else:
-            classes[3].append(new_window_list[i])
-    data = []
-    for count in classes.keys():
-        for wave in classes[count]:
-            if count == 2:
-                i = 3
-                while i:
-                    data.append((wave, count))
-                    i -= 1
-                continue
-            if count > 2:
-                i = 8
-                while i:
-                    data.append((wave, count))
-                    i -= 1
-                continue
-            data.append((wave, count))
-    return data, classes
+                new_window_list.append(self.window_list[i])
+                new_kicks_list.append(self.kick_count_list[i])
+        new_window_list = np.array(new_window_list, dtype=np.float32)
+        new_kicks_list = np.array(new_kicks_list, dtype=np.longlong)
+        return (new_window_list, new_kicks_list)
